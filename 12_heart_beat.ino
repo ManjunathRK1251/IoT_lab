1e87cd01d4e6c0d06b5a18cd9c0ec0ff6ab3d95c
ManjunathRK1251 <rkmanjunath10000@gmail.com>
2022-06-23 21:15:03 +0530

    modify heart beat sensor code

--- 12_heart_beat.ino
+++ 12_heart_beat.ino
@@ -1,19 +1,19 @@
 void setup()
 {
-    pinMode(A0, INPUT);
+    pinMode(AO,INPUT);
     Serial.begin(9600);
 }
 
-void loop()
-{
+void loop(){
     float pulse;
-    int sum = 0;
+    int sum=0;
 
-    for(int i = 0; i < 20; ++i)
-    {
-        sum += analogRead(A0);
-        pulse = sum/20.0;
-        Serial.println(pulse);
-        delay(1000);
+    for(int i=0;i<20;i++)
+	{
+        sum+=analogRead(AO);
     }
+    
+    pulse=sum/20.00;
+    Serial.println(pulse);
+    delay(100);
 }
